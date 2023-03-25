d94af0184f308a006782c95360b8044a89be84e9
Sujipriyanka192003 <123354092+Sujipriyanka192003@users.noreply.github.com>
2023-03-25 13:30:14 +0530

    Add files via upload

---
+++ assessment25/src/Copy_array.java
@@ -0,0 +1,25 @@
+public class Copy_array {
+    public static void main(String[] args) {
+
+        int [] firstArray = new int [] {3, 5, 7, 9, 11};
+
+        int secondArray[] = new int[firstArray.length];
+
+
+        System.out.println("Elements of First array: ");
+        for (int i = 0; i < firstArray.length; i++) {
+            System.out.print(firstArray[i] + " ");
+        }
+
+
+        for (int i = 0; i < firstArray.length; i++) {
+            secondArray[i] = firstArray[i];
+        }
+
+        System.out.println();
+        System.out.println("Elements of Copied array: ");
+        for (int i = 0; i < secondArray.length; i++) {
+            System.out.print(secondArray[i] + " ");
+        }
+    }
+}
